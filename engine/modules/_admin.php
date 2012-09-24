42e8bb4d5f8fca02f33f798cce8e123fe53c6b2a
Ytenok <usuest@gmail.com>
2012-09-24 13:28:09 +0700

    spoiler + div clear

---
+++ engine/modules/_admin.php
@@ -0,0 +1,24 @@
+		<!-- styles needed by jScrollPane - include in your own sites -->
+		<link type="text/css" href="http://rumedia.ws/templates/rum/jscroll/style/jquery.jscrollpane.css" rel="stylesheet" media="all" />
+
+		<style type="text/css" id="page-css">
+			/* Styles specific to this particular page */
+			.scroll-pane
+			{
+				width: 100%;
+				height: 200px;
+				overflow: auto;
+			}
+			.horizontal-only
+			{
+				height: auto;
+				max-height: 200px;
+			}
+		</style>
+
+		<!-- the mousewheel plugin -->
+		<script type="text/javascript" src="http://rumedia.ws/templates/rum/jscroll/script/jquery.mousewheel.js"></script>
+		<!-- the jScrollPane script -->
+		<script type="text/javascript" src="http://rumedia.ws/templates/rum/jscroll/script/jquery.jscrollpane.min.js"></script>
+	
+
